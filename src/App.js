3131f27a06cdf1dea7c3d49f49dd5ae9e41aabaf
Mick Ryan <mickryan@box.com>
2019-10-24 15:23:45 -0700

    Changing for demo application

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Footer from './Footer';
 import Nav from './Nav';
 import './app.scss';
 import { IntlProvider } from 'react-intl';
-import { ContentExplorer } from 'box-ui-elements';
+import { ContentPreview } from 'box-ui-elements';
 
 // Read only token used in the demo sites
 const FOLDER_ID = '69083462919';
-const FILE_ID = '415542803939';
-const TOKEN = 'S8wjvjOL9GEK5VtXsQNVMOwSrx1g55oC';
+const FILE_ID = '545680293326';
+const TOKEN = 'Es7LOQiu6VJPBapCcCnpVF4mYaooef8B';
 const FEATURES = {
     activityFeed: {
         tasks: {
@@ -28,10 +28,9 @@ const PROPS = {};
 
 const App = () => (
     <div className='app'>
-        <Header />
         <div className='body'>
             <IntlProvider locale="en" textComponent={React.Fragment}>
-                <ContentExplorer
+                <ContentPreview
                     contentSidebarProps={{
                         detailsSidebarProps: {
                             hasAccessStats: true,
@@ -55,9 +54,7 @@ const App = () => (
                 />
             </IntlProvider>
             {/*<main className={className}>{children}</main>*/}
-            <Nav />
         </div>
-        <Footer />
     </div>
 );
 
